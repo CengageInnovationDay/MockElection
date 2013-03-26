40770303ee2d0ba094f7fb9cf49724632fa98482
numone <christinjhansen@gmail.com>
2013-03-26 12:49:17 -0400

    adding votes needed to win and renaming the css file

--- WebContent/src/SelectionBar.js
+++ WebContent/src/SelectionBar.js
@@ -1,6 +1,7 @@
 Jax5.SelectionBar = function(totalVotes){
     $('#svgHolder').prepend(this.getBars());
     this.totalVotes = totalVotes || 0;
+    this.showVotesNeeded();
 };
 
 Jax5.SelectionBar.prototype = {
@@ -12,10 +13,14 @@ Jax5.SelectionBar.prototype = {
         },
         setTotalVotes: function(totalVotes){
             this.totalVotes = totalVotes;
+            this.showVotesNeeded();
+        },
+        showVotesNeeded: function(){
+            $('#votesNeededArea').text(Math.ceil(this.totalVotes/2));
         },
         getBars: function(){
             return '<div id="votesBar" class="ui-progressbar ui-widget ui-widget-content ui-corner-all">' +
-                '<div id="republicanBar"></div><div id="democraticBar">' +
-                '</div></div><div id="middlePointsBar"></div>';
+                '<div id="republicanBar"></div><div id="democraticBar"></div>' +
+                '</div><div id="middlePointsBar"></div><div id="votesNeededArea"></div>';
         }
 };
