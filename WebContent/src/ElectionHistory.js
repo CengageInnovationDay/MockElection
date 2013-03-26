2bc2ad4a576adb867a09fb9536d69d0cabd3f2c8
Chris Hansen <christinjhansen@gmail.com>
2013-03-26 00:33:55 -0400

    -moved the svg into its own prototype class -fixed the json syntax of the election history -started working on a UI for election history

---
+++ WebContent/src/ElectionHistory.js
@@ -0,0 +1,43 @@
+Jax5.ElectionHistory = function () {
+	new Jax5.DrawSVG();
+	$('#svgHolder').append(this.historyHolder());
+    this.$div = $('#electionHisDiv');
+    this.div = this.$div.get(0);
+    this.loadHistory();
+};
+
+Jax5.ElectionHistory.prototype = {
+		historyHolder: function() {
+			return '<div id="electionHisDiv" style="width:200px;border:1px dotted green;float:left;"></div>';
+		},
+		loadHistory: function(){
+			var that = this;
+			$.ajax('src/PresidentalElectionHistory.json', {
+				dataType: 'json',
+				success: function(data, status, xhr) {
+					that.showHistory(data);
+				},
+				error: function(xhr, status, errorThrown) {
+					console.log('Error loading PresidentalElectionHistory.json: ' + errorThrown);
+				}
+			});
+		},
+		showHistory: function(data){
+			var that = this;
+			$(data.years).each(function(){
+				if(data[this]){
+					that.showHistoricalYear(data[this],this);
+				}
+			});
+		},
+		showHistoricalYear: function(data,year){
+			this.$div.append('<div>' + year + ':<br />' + this.listCandidates(data.candidates));
+		},
+		listCandidates: function(candidates){
+			var toReturn = '';
+			$(candidates).each(function(){
+				toReturn = toReturn + this.name + ' (' + this.party + ')<br />';
+			});
+			return toReturn;
+		}
+};
