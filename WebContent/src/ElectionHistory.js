c5181ac228800aa23329e23357d698a13a45ad62
numone <christinjhansen@gmail.com>
2013-03-26 09:23:22 -0400

    switching ajax call to included file to get around security concerns in chrome

--- WebContent/src/ElectionHistory.js
+++ WebContent/src/ElectionHistory.js
@@ -3,27 +3,29 @@ Jax5.ElectionHistory = function () {
 	$('#svgHolder').append(this.historyHolder());
     this.$div = $('#electionHisDiv');
     this.div = this.$div.get(0);
-    this.loadHistory();
+    //this.loadHistory();
+    this.showHistory();
 };
 
 Jax5.ElectionHistory.prototype = {
 		historyHolder: function() {
 			return '<div id="electionHisDiv" style="width:200px;border:1px dotted green;float:left;"></div>';
 		},
-		loadHistory: function(){
+//		loadHistory: function(){
+//			var that = this;
+//			$.ajax('src/PresidentalElectionHistory.json', {
+//				dataType: 'json',
+//				success: function(data, status, xhr) {
+//					that.showHistory(data);
+//				},
+//				error: function(xhr, status, errorThrown) {
+//					console.log('Error loading PresidentalElectionHistory.json: ' + errorThrown);
+//				}
+//			});
+//		},
+		showHistory: function(/*data*/){
 			var that = this;
-			$.ajax('src/PresidentalElectionHistory.json', {
-				dataType: 'json',
-				success: function(data, status, xhr) {
-					that.showHistory(data);
-				},
-				error: function(xhr, status, errorThrown) {
-					console.log('Error loading PresidentalElectionHistory.json: ' + errorThrown);
-				}
-			});
-		},
-		showHistory: function(data){
-			var that = this;
+			var data = Jax5.HistoryObject;
 			$(data.years).each(function(){
 				if(data[this]){
 					that.showHistoricalYear(data[this],this);
